133c43876a1ecb4ba37002348e7c1e9c7d872df0
Paul Bourke <pauldbourke@gmail.com>
2012-08-07 23:42:02 +0100

    Add pagination to GroupPoolGridFragment
    Also:
    * Update to latest version of suppport lib in ViewPagerIndicator and
      ActionBarSherlock to avoid http://stackoverflow.com/a/9021487/663370
    
    * Comment Constants.java
    
    * setRetainInstance(true) on BaseFragment which makes rotation better -
      though still have a problem with User overlay not retaining.

--- src/com/bourke/glimmr/common/Constants.java
+++ src/com/bourke/glimmr/common/Constants.java
@@ -2,10 +2,17 @@ package com.bourke.glimmr.common;
 
 public class Constants {
 
+    /* File and mem cache tuning params */
     public static final boolean USE_FILE_CACHE = true;
     public static final boolean USE_MEMORY_CACHE = true;
 
+    /* Number of items to fetch per page for calls that support pagination */
+    public static final int FETCH_PER_PAGE = 20;
+
+    /* OAuth callback uri */
     public static final String CALLBACK_SCHEME = "glimmr-oauth-callback";
+
+    /* Global app prefs */
     public static final String PREFS_NAME = "glimmr_prefs";
 
     public static final String KEY_OAUTH_TOKEN = "glimmr_oauthtoken";
@@ -13,6 +20,7 @@ public class Constants {
     public static final String KEY_USER_NAME = "glimmr_username";
     public static final String KEY_USER_ID = "glimmr_userid";
 
+    /* Photo viewer state */
     public static final String KEY_PHOTOVIEWER_LIST = "glimmr_photolist";
     public static final String KEY_PHOTOVIEWER_URL = "glimmr_photoviewer_url";
     public static final String KEY_PHOTOVIEWER_START_INDEX =
@@ -20,19 +28,23 @@ public class Constants {
     public static final String KEY_PROFILEVIEWER_USER =
         "glimmr_profileviewer_user";
 
+    /* Group viewer state */
     public static final String KEY_GROUPVIEWER_GROUP =
         "glimmr_groupviewer_group";
     public static final String KEY_GROUPVIEWER_USER =
         "glimmr_groupviewer_user";
 
+    /* Photoset viewer state */
     public static final String KEY_PHOTOSETVIEWER_PHOTOSET =
         "glimmr_photosetviewer_photoset";
     public static final String KEY_PHOTOSETVIEWER_USER =
         "glimmr_photosetviewer_user";
 
+    /* Exif dialog state */
     public static final String KEY_EXIF_INFO_DIALOG_ACTIVITY_PHOTO =
         "glimmr_exif_info_dialog_activity_photo";
 
+    /* Comments dialog state */
     public static final String COMMENTS_DIALOG_ACTIVITY_PHOTO =
         "glimmr_comments_dialog_activity_photo";
 }
