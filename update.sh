4c511f780572c71e21103ae76be8dd3ce6bca8d7
Paul Bourke <pauldbourke@gmail.com>
2012-09-14 10:41:13 +0100

    Update aquery and ViewPagerIndicator
    android-query: 0.23.16
    ViewPagerIndicator: 2.4.1

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 android update lib-project -p \
-    libs/JakeWharton-Android-ViewPagerIndicator-98a5218/library/ -t android-16
+    libs/JakeWharton-ActionBarSherlock-88fc341/library/ -t android-14
 
 android update lib-project -p \
-    libs/JakeWharton-ActionBarSherlock-88fc341/library/ -t android-14
+    libs/JakeWharton-Android-ViewPagerIndicator-8cd549f/library/ -t android-16
 
 android update lib-project -p \
     libs/appirater-android-f92df01f/ -t android-3
