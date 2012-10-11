5452e1f843ec97e0c71a2b7ee12793d45555ac45
Paul Bourke <pauldbourke@gmail.com>
2012-10-11 14:10:43 +0100

    Update actionbarsherlock and appirater to latest

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 android update lib-project -p \
-    libs/JakeWharton-ActionBarSherlock-aece21a/library/ -t android-14
+    libs/JakeWharton-ActionBarSherlock-e5c2d1c/library/ -t android-14
 
 android update lib-project -p \
     libs/JakeWharton-Android-ViewPagerIndicator-8cd549f/library/ -t android-16
 
 android update lib-project -p \
-    libs/appirater-android-6a954f19/ -t android-3
+    libs/appirater-android/ -t android-3
 
 android update project -p . -t android-16
 
