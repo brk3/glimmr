4bd58d4b7dab258b515274fd1e36deb2bfca8a25
Paul Bourke <pauldbourke@gmail.com>
2012-10-13 19:07:54 +0100

    Update profile page with tabs for tablets
    Conflicts:
    
    	src/com/bourke/glimmr/activities/ProfileActivity.java

--- src/com/bourke/glimmr/activities/MainActivity.java
+++ src/com/bourke/glimmr/activities/MainActivity.java
@@ -8,23 +8,20 @@ import android.os.Bundle;
 
 import android.preference.PreferenceManager;
 
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 
 import android.util.Log;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragment;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 import com.androidquery.AQuery;
 
 import com.bourke.glimmrpro.common.Constants;
+import com.bourke.glimmrpro.common.GlimmrTabsAdapter;
 import com.bourke.glimmrpro.fragments.explore.RecentPublicPhotosFragment;
 import com.bourke.glimmrpro.fragments.home.ContactsGridFragment;
 import com.bourke.glimmrpro.fragments.home.GroupListFragment;
@@ -41,8 +38,6 @@ import com.googlecode.flickrjandroid.people.User;
 import com.sbstrm.appirater.Appirater;
 
 import com.viewpagerindicator.TitlePageIndicator;
-
-import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
 
@@ -178,85 +173,6 @@ public class MainActivity extends BaseActivity {
         public CharSequence getPageTitle(int position) {
             return MainActivity.CONTENT[position % MainActivity.CONTENT.length]
                 .toUpperCase();
-        }
-    }
-
-    class GlimmrTabsAdapter extends FragmentPagerAdapter implements
-            ActionBar.TabListener, ViewPager.OnPageChangeListener {
-        private final Context mContext;
-        private final ActionBar mActionBar;
-        private final ViewPager mViewPager;
-        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-
-        public GlimmrTabsAdapter(SherlockFragmentActivity activity,
-                ViewPager pager) {
-            super(activity.getSupportFragmentManager());
-            mContext = activity;
-            mActionBar = activity.getSupportActionBar();
-            mViewPager = pager;
-        }
-
-        public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-            TabInfo info = new TabInfo(clss, args);
-            tab.setTag(info);
-            tab.setTabListener(this);
-            mTabs.add(info);
-            mActionBar.addTab(tab);
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getCount() {
-            return mTabs.size();
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            TabInfo info = mTabs.get(position);
-            return Fragment.instantiate(mContext, info.clss.getName(),
-                    info.args);
-        }
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset,
-                int positionOffsetPixels) {
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            mActionBar.setSelectedNavigationItem(position);
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-        }
-
-        @Override
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            Object tag = tab.getTag();
-            for (int i = 0; i < mTabs.size(); i++) {
-                if (mTabs.get(i) == tag) {
-                    mViewPager.setCurrentItem(i);
-                }
-            }
-        }
-
-        @Override
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-        }
-
-        @Override
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
-        }
-
-        final class TabInfo {
-            private final Class<?> clss;
-            private final Bundle args;
-
-            TabInfo(Class<?> _class, Bundle _args) {
-                clss = _class;
-                args = _args;
-            }
         }
     }
 }
