803904b4547eba55386234082de4c281eaf34c39
Paul Bourke <pauldbourke@gmail.com>
2012-10-20 15:24:00 +0100

    Add search functionality

---
+++ src/com/bourke/glimmr/tasks/SearchPhotosTask.java
@@ -0,0 +1,98 @@
+package com.bourke.glimmr.tasks;
+
+import android.os.AsyncTask;
+
+import android.util.Log;
+
+import com.bourke.glimmr.common.Constants;
+import com.bourke.glimmr.common.FlickrHelper;
+import com.bourke.glimmr.event.Events.IPhotoListReadyListener;
+import com.bourke.glimmr.fragments.base.BaseFragment;
+
+import com.googlecode.flickrjandroid.Flickr;
+import com.googlecode.flickrjandroid.oauth.OAuth;
+import com.googlecode.flickrjandroid.oauth.OAuthToken;
+import com.googlecode.flickrjandroid.photosets.Photoset;
+import com.googlecode.flickrjandroid.photos.PhotoList;
+import com.googlecode.flickrjandroid.photos.SearchParameters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SearchPhotosTask extends AsyncTask<OAuth, Void, PhotoList> {
+
+    private static final String TAG = "Glimmr/SearchPhotosTask";
+
+    private IPhotoListReadyListener mListener;
+    private Photoset mPhotoset;
+    private BaseFragment mBaseFragment;
+    private int mPage;
+    private String mSearchTerm;
+
+    public SearchPhotosTask(BaseFragment a, IPhotoListReadyListener listener,
+            String searchTerm, int page) {
+        mBaseFragment = a;
+        mListener = listener;
+        mPage = page;
+        mSearchTerm = searchTerm;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mBaseFragment.showProgressIcon(true);
+    }
+
+    @Override
+    protected PhotoList doInBackground(OAuth... params) {
+        OAuth oauth = params[0];
+        Set<String> extras = new HashSet<String>();
+        extras.add("owner_name");
+        extras.add("url_q");
+        extras.add("url_l");
+        extras.add("views");
+
+        SearchParameters sp = new SearchParameters();
+        sp.setExtras(extras);
+        sp.setText(mSearchTerm);
+        sp.setSort(SearchParameters.RELEVANCE);
+
+        if (oauth != null) {
+            OAuthToken token = oauth.getToken();
+            Flickr f = FlickrHelper.getInstance().getFlickrAuthed(
+                    token.getOauthToken(), token.getOauthTokenSecret());
+            if (Constants.DEBUG) Log.d(TAG, "Fetching page " + mPage);
+            try {
+                return f.getPhotosInterface().search(
+                        sp, Constants.FETCH_PER_PAGE, mPage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            if (Constants.DEBUG) Log.d(TAG, "Making unauthenticated call");
+            if (Constants.DEBUG) Log.d(TAG, "Fetching page " + mPage);
+            try {
+                return FlickrHelper.getInstance().getPhotosInterface()
+                    .search(sp, Constants.FETCH_PER_PAGE, mPage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(PhotoList result) {
+        if (result == null) {
+            Log.e(TAG, "Error fetching photolist, result is null");
+            result = new PhotoList();
+        }
+        mListener.onPhotosReady(result);
+        mBaseFragment.showProgressIcon(false);
+    }
+
+    @Override
+    protected void onCancelled(final PhotoList result) {
+        if (Constants.DEBUG) Log.d(TAG, "onCancelled");
+    }
+}
