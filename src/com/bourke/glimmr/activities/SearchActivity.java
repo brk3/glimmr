b6f7c6f5e649fc1173f1ec24e9b9e043668daf56
Paul Bourke <pauldbourke@gmail.com>
2012-10-20 15:24:00 +0100

    Add search functionality

---
+++ src/com/bourke/glimmr/activities/SearchActivity.java
@@ -0,0 +1,74 @@
+package com.bourke.glimmr.activities;
+
+import android.app.SearchManager;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.support.v4.view.ViewPager;
+
+import android.util.Log;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+import com.bourke.glimmr.common.Constants;
+import com.bourke.glimmr.common.GlimmrPagerAdapter;
+import com.bourke.glimmr.fragments.search.PhotoSearchGridFragment;
+import com.bourke.glimmr.R;
+
+public class SearchActivity extends BottomOverlayActivity {
+
+    private static final String TAG = "Glimmr/SearchActivity";
+
+    public static final int RESULT_PHOTO_PAGE = 0;
+    public static final int RESULT_PEOPLE_PAGE = 1;
+
+    private String mSearchQuery;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        CONTENT = new String[] { getString(R.string.photos) };
+            //getString(R.string.people) };
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            mSearchQuery = intent.getStringExtra(SearchManager.QUERY);
+            if (Constants.DEBUG) {
+                Log.d(TAG, String.format("Got search query: '%s'",
+                            mSearchQuery));
+            }
+            initViewPager();
+        }
+    }
+
+    @Override
+    protected void initViewPager() {
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mAdapter = new GlimmrPagerAdapter(getSupportFragmentManager(),
+                mViewPager, mActionBar, CONTENT) {
+            @Override
+            public SherlockFragment getItemImpl(int position) {
+                switch (position) {
+                    case RESULT_PHOTO_PAGE:
+                        return PhotoSearchGridFragment
+                            .newInstance(mSearchQuery);
+                    case RESULT_PEOPLE_PAGE:
+                        // TODO
+                        //return PeopleSearchFragment.
+                        //    .newInstance(mSearchQuery);
+                        break;
+                }
+                return null;
+            }
+        };
+        super.initViewPager();
+    }
+
+    @Override
+    protected void updateBottomOverlay() {
+    }
+}
---
+++ src/com/bourke/glimmr/fragments/search/PhotoSearchGridFragment.java
@@ -0,0 +1,85 @@
+package com.bourke.glimmr.fragments.search;
+
+import android.os.Bundle;
+
+import com.bourke.glimmr.event.Events.IPhotoListReadyListener;
+import com.bourke.glimmr.fragments.base.PhotoGridFragment;
+import com.bourke.glimmr.R;
+import com.bourke.glimmr.tasks.SearchPhotosTask;
+
+import com.googlecode.flickrjandroid.photos.Photo;
+import com.googlecode.flickrjandroid.photos.PhotoList;
+
+public class PhotoSearchGridFragment extends PhotoGridFragment
+        implements IPhotoListReadyListener {
+
+    private static final String TAG = "Glimmr/PhotoSearchGridFragment";
+
+    private SearchPhotosTask mTask;
+    private String mSearchTerm;
+
+    public static PhotoSearchGridFragment newInstance(String searchTerm) {
+        PhotoSearchGridFragment fragment = new PhotoSearchGridFragment();
+        fragment.mSearchTerm = searchTerm;
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mShowDetailsOverlay = false;
+    }
+
+    /**
+     * Once the parent binds the adapter it will trigger cacheInBackground
+     * for us as it will be empty when first bound.
+     */
+    @Override
+    protected boolean cacheInBackground() {
+        startTask(mPage++);
+        return mMorePages;
+    }
+
+    private void startTask(int page) {
+        super.startTask();
+        mTask = new SearchPhotosTask(this, this, mSearchTerm, page);
+        mTask.execute(mOAuth);
+    }
+
+    @Override
+    protected void refresh() {
+        super.refresh();
+        mTask = new SearchPhotosTask(this, this, mSearchTerm, mPage);
+        mTask.execute(mOAuth);
+    }
+
+    @Override
+    public void onPhotosReady(PhotoList photos) {
+        super.onPhotosReady(photos);
+        if (photos != null && photos.isEmpty()) {
+            mMorePages = false;
+
+            /* If first page (2 as mPage will have already been incremented),
+             * and results are empty, show no search results layout */
+            if (mPage == 2) {
+                mAq.id(R.id.no_search_results_layout).visible();
+                mAq.id(R.id.gridview).gone();
+            }
+        }
+    }
+
+    @Override
+    public String getNewestPhotoId() {
+        /* Not needed for this fragment */
+        return null;
+    }
+
+    @Override
+    public void storeNewestPhotoId(Photo photo) {
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+}
