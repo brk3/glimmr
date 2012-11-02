97cfe707529265be870b91c85806dcc011e25c8e
Paul Bourke <pauldbourke@gmail.com>
2012-11-02 00:51:41 +0000

    Add android-menudrawer
    Will allow as a pattern to add more features without cluttering the
    interface.  New comments/favorites stream may also end up here.
    
    Also:
    * Clean up some unneeded resources.
    
    Conflicts:
    
    	src/com/bourke/glimmr/activities/MainActivity.java

--- src/com/bourke/glimmr/activities/MainActivity.java
+++ src/com/bourke/glimmr/activities/MainActivity.java
@@ -8,20 +8,30 @@ import android.os.Bundle;
 
 import android.preference.PreferenceManager;
 
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import android.util.Log;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
-import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.MenuItem;
 
 import com.androidquery.AQuery;
 
 import com.bourke.glimmrpro.common.Constants;
 import com.bourke.glimmrpro.common.GlimmrPagerAdapter;
+import com.bourke.glimmrpro.common.MenuListView;
 import com.bourke.glimmrpro.fragments.explore.RecentPublicPhotosFragment;
 import com.bourke.glimmrpro.fragments.home.ContactsGridFragment;
+import com.bourke.glimmrpro.fragments.home.FavoritesGridFragment;
 import com.bourke.glimmrpro.fragments.home.GroupListFragment;
 import com.bourke.glimmrpro.fragments.home.PhotosetsFragment;
 import com.bourke.glimmrpro.fragments.home.PhotoStreamGridFragment;
@@ -35,36 +45,47 @@ import com.googlecode.flickrjandroid.people.User;
 
 import com.sbstrm.appirater.Appirater;
 
+import com.viewpagerindicator.PageIndicator;
 import com.viewpagerindicator.TitlePageIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.simonvt.widget.MenuDrawer;
+import net.simonvt.widget.MenuDrawerManager;
 
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = "Glimmr/MainActivity";
 
-    public static final int CONTACTS_PAGE = 0;
-    public static final int PHOTOSTREAM_PAGE = 1;
-    public static final int SETS_PAGE = 2;
-    public static final int GROUPS_PAGE = 3;
-    public static final int EXPLORE_PAGE = 4;
+    private List<PageItem> mContent;
 
-    public static String[] CONTENT;
+    private MenuDrawerManager mMenuDrawer;
+    private MenuAdapter mMenuAdapter;
+    private MenuListView mList;
+    private PagerAdapter mPagerAdapter;
+    private ViewPager mViewPager;
+    private PageIndicator mIndicator;
+    private int mActivePosition = -1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Constants.DEBUG) Log.d(getLogTag(), "onCreate");
-
-        CONTENT = new String[] { getString(R.string.contacts),
-            getString(R.string.you), getString(R.string.sets),
-            getString(R.string.groups), getString(R.string.explore)
-        };
-
         if (mOAuth == null) {
             startActivity(new Intent(this, ExploreActivity.class));
         } else {
-            setContentView(R.layout.main_activity);
+            if (savedInstanceState != null) {
+                mActivePosition =
+                    savedInstanceState.getInt(Constants.STATE_ACTIVE_POSITION);
+            }
             mAq = new AQuery(this);
+            initPageItems();
+            mMenuDrawer =
+                new MenuDrawerManager(this, MenuDrawer.MENU_DRAG_CONTENT);
+            mMenuDrawer.setContentView(R.layout.main_activity);
             initViewPager();
+            initMenuDrawer();
             initNotificationAlarms();
             Appirater.appLaunched(this);
         }
@@ -79,6 +100,132 @@ public class MainActivity extends BaseActivity {
         if (mOAuth != null) {
             mUser = mOAuth.getUser();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            mMenuDrawer.toggleMenu();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        final int drawerState = mMenuDrawer.getDrawerState();
+        if (drawerState == MenuDrawer.STATE_OPEN ||
+                drawerState == MenuDrawer.STATE_OPENING) {
+            mMenuDrawer.closeMenu();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(Constants.STATE_MENUDRAWER,
+                mMenuDrawer.onSaveDrawerState());
+        outState.putInt(Constants.STATE_ACTIVE_POSITION, mActivePosition);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle inState) {
+        super.onRestoreInstanceState(inState);
+        mMenuDrawer.onRestoreDrawerState(inState
+                .getParcelable(Constants.STATE_MENUDRAWER));
+    }
+
+    @Override
+    public User getUser() {
+        return mUser;
+    }
+
+    private void initPageItems() {
+        mContent = new ArrayList<PageItem>();
+
+        mContent.add(new PageItem(getString(R.string.contacts),
+                R.drawable.ic_action_social_person_dark,
+                ContactsGridFragment.class));
+
+        mContent.add(new PageItem(getString(R.string.photos),
+                R.drawable.ic_content_picture_dark,
+                PhotoStreamGridFragment.class));
+
+        mContent.add(new PageItem(getString(R.string.favorites),
+                R.drawable.ic_action_rating_important_dark,
+                FavoritesGridFragment.class));
+
+        mContent.add(new PageItem(getString(R.string.sets),
+                R.drawable.collections_collection_dark,
+                PhotosetsFragment.class));
+
+        mContent.add(new PageItem(getString(R.string.groups),
+                R.drawable.ic_action_social_group_dark,
+                GroupListFragment.class));
+
+        mContent.add(new PageItem(getString(R.string.explore),
+                R.drawable.ic_action_av_shuffle_dark,
+                RecentPublicPhotosFragment.class));
+    }
+
+    private void initMenuDrawer() {
+        List<Object> items = new ArrayList<Object>();
+        for (PageItem page : mContent) {
+            items.add(new Item(page.mTitle, page.mIconDrawable));
+        }
+        items.add(new Category(
+                getString(R.string.pref_category_notifications)));
+        //items.add(new Item("Item 3", R.drawable.ic_action_refresh_dark));
+
+        /* A custom ListView is needed so the drawer can be notified when it's
+         * scrolled. This is to update the position
+         * of the arrow indicator. */
+        mList = new MenuListView(this);
+        mMenuAdapter = new MenuAdapter(items);
+        mList.setAdapter(mMenuAdapter);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                mViewPager.setCurrentItem(position);
+                mActivePosition = position;
+                mMenuDrawer.setActiveView(view, position);
+                mMenuDrawer.closeMenu();
+            }
+        });
+
+        mList.setOnScrollChangedListener(
+                new MenuListView.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                mMenuDrawer.getMenuDrawer().invalidate();
+            }
+        });
+
+        mMenuDrawer.setMenuView(mList);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        mMenuDrawer.getMenuDrawer().setTouchMode(
+                MenuDrawer.TOUCH_MODE_FULLSCREEN);
+
+        mIndicator.setOnPageChangeListener(
+                new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(final int position) {
+                mIndicator.setCurrentItem(position);
+                if (position == 0) {
+                    mMenuDrawer.getMenuDrawer().setTouchMode(
+                        MenuDrawer.TOUCH_MODE_FULLSCREEN);
+                } else {
+                    mMenuDrawer.getMenuDrawer().setTouchMode(
+                        MenuDrawer.TOUCH_MODE_NONE);
+                }
+            }
+        });
     }
 
     private void initNotificationAlarms() {
@@ -97,49 +244,148 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initViewPager() {
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        List<String> pageTitles = new ArrayList<String>();
+        for (PageItem page : mContent) {
+            pageTitles.add(page.mTitle);
+        }
         GlimmrPagerAdapter adapter = new GlimmrPagerAdapter(
-                getSupportFragmentManager(), viewPager, mActionBar, CONTENT) {
+                getSupportFragmentManager(), mViewPager, mActionBar,
+                pageTitles.toArray(new String[pageTitles.size()])) {
             @Override
             public SherlockFragment getItemImpl(int position) {
-                switch (position) {
-                    case PHOTOSTREAM_PAGE:
-                        return PhotoStreamGridFragment.newInstance();
-
-                    case CONTACTS_PAGE:
-                        return ContactsGridFragment.newInstance();
-
-                    case GROUPS_PAGE:
-                        return GroupListFragment.newInstance();
-
-                    case SETS_PAGE:
-                        return PhotosetsFragment.newInstance();
-
-                    case EXPLORE_PAGE:
-                        return RecentPublicPhotosFragment.newInstance();
+                try {
+                    return (SherlockFragment)
+                        mContent.get(position).mFragmentClass.newInstance();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
                 }
                 return null;
             }
         };
-        viewPager.setAdapter(adapter);
+        mViewPager.setAdapter(adapter);
 
-        TitlePageIndicator indicator =
-            (TitlePageIndicator) findViewById(R.id.indicator);
-        if (indicator != null) {
-            indicator.setViewPager(viewPager);
+        mIndicator = (TitlePageIndicator) findViewById(R.id.indicator);
+        if (mIndicator != null) {
+            mIndicator.setViewPager(mViewPager);
         } else {
             mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-            viewPager.setOnPageChangeListener(adapter);
-            for (String title : CONTENT) {
-                ActionBar.Tab newTab = mActionBar.newTab().setText(title);
+            mViewPager.setOnPageChangeListener(adapter);
+            for (PageItem page : mContent) {
+                ActionBar.Tab newTab =
+                    mActionBar.newTab().setText(page.mTitle);
                 newTab.setTabListener(adapter);
                 mActionBar.addTab(newTab);
             }
         }
     }
 
-    @Override
-    public User getUser() {
-        return mUser;
+    private static final class PageItem {
+        public String mTitle;
+        public Integer mIconDrawable;
+        public Class mFragmentClass;
+
+        PageItem(String title, int iconDrawable, Class fragmentClass) {
+            mTitle = title;
+            mIconDrawable = iconDrawable;
+            mFragmentClass = fragmentClass;
+        }
+    }
+
+    private static final class Item {
+        public String mTitle;
+        public int mIconRes;
+
+        Item(String title, int iconRes) {
+            mTitle = title;
+            mIconRes = iconRes;
+        }
+    }
+
+    private static final class Category {
+        public String mTitle;
+
+        Category(String title) {
+            mTitle = title;
+        }
+    }
+
+    private class MenuAdapter extends BaseAdapter {
+        private List<Object> mItems;
+
+        MenuAdapter(List<Object> items) {
+            mItems = items;
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return getItem(position) instanceof Item ? 0 : 1;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return getItem(position) instanceof Item;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            Object item = getItem(position);
+
+            if (item instanceof Category) {
+                if (v == null) {
+                    v = getLayoutInflater().inflate(R.layout.menu_row_category,
+                            parent, false);
+                }
+
+                ((TextView) v).setText(((Category) item).mTitle);
+
+            } else {
+                if (v == null) {
+                    v = getLayoutInflater().inflate(R.layout.menu_row_item,
+                            parent, false);
+                }
+
+                TextView tv = (TextView) v;
+                tv.setText(((Item) item).mTitle);
+                tv.setCompoundDrawablesWithIntrinsicBounds(
+                        ((Item) item).mIconRes, 0, 0, 0);
+            }
+
+            v.setTag(R.id.mdActiveViewPosition, position);
+
+            if (position == mActivePosition) {
+                mMenuDrawer.setActiveView(v, position);
+            }
+
+            return v;
+        }
     }
 }
