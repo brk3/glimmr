2e40ad59ffc3e431510ec18bb46cbb9927f47992
Paul Bourke <pauldbourke@gmail.com>
2012-11-03 14:42:11 +0000

    Refactor AppService to support multiple handlers
    Define an interface for a handler that can be run periodically via
    AppService.  This allows for just one Service that can delagate it's
    work.
    
    Also makes way for showing flickr activity items in the menu drawer.
    They are currently fetched via an AppService handler, and stored to
    disk for MainActivity to read.
    
    Next steps should be:
    * Define a new row type to display activity items in the MenuDrawer.
    
    * Provide a way to refresh these manually, as well as the background
      service refreshing them periodically.
    
    Also:
    * Add commit id to CWACWakeful jar
    
    * Update flickrj-android jar with bug fix for Activity and make activity
      classes serializable

---
+++ src/com/bourke/glimmr/services/GlimmrPhotoNotificationHandler.java
@@ -0,0 +1,19 @@
+package com.bourke.glimmr.services;
+
+import com.googlecode.flickrjandroid.photos.Photo;
+import com.googlecode.flickrjandroid.photos.PhotoList;
+
+import java.util.List;
+
+public interface GlimmrPhotoNotificationHandler {
+
+    public void showNewPhotosNotification(List<Photo> newPhotos);
+
+    public List<Photo> checkForNewPhotos(PhotoList photos);
+
+    public String getNewestViewedPhotoId();
+
+    public String getNewestNotificationPhotoId();
+
+    public void storeNewestNotificationPhotoId(Photo photo);
+}
