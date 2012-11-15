c729c7239c50f0f4e15b10ea3e32450f0e316089
Paul Bourke <pauldbourke@gmail.com>
2012-11-15 17:59:28 +0000

    Flash the up button on first run
    This encourages users to find out about the menu drawer if they don't
    know already.

--- src/com/bourke/glimmr/common/Constants.java
+++ src/com/bourke/glimmr/common/Constants.java
@@ -19,6 +19,8 @@ public class Constants {
     public static final String KEY_USER_NAME = "glimmr_user_name";
     public static final String KEY_USER_ID = "glimmr_user_id";
 
+    public static final String KEY_IS_FIRST_RUN = "glimmr_is_first_run";
+
     /* Fonts */
     public static final int FONT_SHADOWSINTOLIGHT = 0;
     public static final String FONT_PATH_SHADOWSINTOLIGHT =
