baac18172049550c601f7962ca586e48c081ff6f
Paul Bourke <pauldbourke@gmail.com>
2012-11-26 21:17:54 +0000

    Fix calling checkForNewItemEvents with empty list

--- src/com/bourke/glimmr/services/ActivityNotificationHandler.java
+++ src/com/bourke/glimmr/services/ActivityNotificationHandler.java
@@ -83,7 +83,7 @@ public class ActivityNotificationHandler
 
     @Override
     public void onItemListReady(List<Item> items) {
-        if (items != null) {
+        if (items != null && !items.isEmpty()) {
             if (Constants.DEBUG) {
                 Log.d(TAG, "onItemListReady: items.size: " + items.size());
             }
