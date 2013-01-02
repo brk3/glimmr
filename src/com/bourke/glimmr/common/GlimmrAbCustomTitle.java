6b1c937d182fbb5665da00b18f15be7cdaaa6d13
Paul Bourke <pauldbourke@gmail.com>
2013-01-02 16:12:04 +0000

    Add dropdown to SearchActivity to enable sorting
    Conflicts:
    	src/com/bourke/glimmr/common/GlimmrAbCustomTitle.java
    	src/com/bourke/glimmr/fragments/search/PhotoSearchGridFragment.java
    	src/com/bourke/glimmr/tasks/SearchPhotosTask.java

--- src/com/bourke/glimmr/common/GlimmrAbCustomTitle.java
+++ src/com/bourke/glimmr/common/GlimmrAbCustomTitle.java
@@ -33,17 +33,10 @@ public class GlimmrAbCustomTitle {
             mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = inflator.inflate(R.layout.action_bar_title_item, null);
         mAbTitle = (TextView) v.findViewById(R.id.abTitle);
-
-        /* NOTE: Large screens don't use a custom title in landscape as there's
-         * a problem/bug where it sits to the right of the tabs */
-        if (mAbTitle != null) {
-            mTextUtils.setFont(mAbTitle, TextUtils.FONT_SHADOWSINTOLIGHT);
-            mAbTitle.setText(mActivity.getString(R.string.app_name));
-            actionbar.setDisplayShowCustomEnabled(true);
-            actionbar.setDisplayShowTitleEnabled(false);
-            actionbar.setCustomView(v);
-        } else {
-            actionbar.setTitle("");
-        }
+        mTextUtils.setFont(mAbTitle, TextUtils.FONT_SHADOWSINTOLIGHT);
+        mAbTitle.setText(mActivity.getString(R.string.app_name));
+        actionbar.setDisplayShowCustomEnabled(true);
+        actionbar.setDisplayShowTitleEnabled(false);
+        actionbar.setCustomView(v);
     }
 }
