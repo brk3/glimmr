5054c7f35f787e1cdc32409ca0941473a5b57c15
Paul Bourke <pauldbourke@gmail.com>
2013-05-20 01:00:00 +0100

    Update android-menudrawer to add new indicator
    Also update ABS to 4.3.1

--- src/main/java/com/bourke/glimmr/activities/MainActivity.java
+++ src/main/java/com/bourke/glimmr/activities/MainActivity.java
@@ -295,6 +295,9 @@ public class MainActivity extends BaseActivity {
     private void initMenuDrawer() {
         mMenuDrawer = MenuDrawer.attach(this, MenuDrawer.MENU_DRAG_CONTENT);
         mMenuDrawer.setContentView(R.layout.main_activity);
+        /* The drawable that replaces the up indicator in the action bar */
+        mMenuDrawer.setSlideDrawable(R.drawable.ic_drawer);
+        mMenuDrawer.setDrawerIndicatorEnabled(true);
         MenuListView menuListView = new MenuListView(this);
         menuListView.setDivider(null);
         menuListView.setDividerHeight(0);
@@ -350,6 +353,10 @@ public class MainActivity extends BaseActivity {
                                 updateMenuListItems(false);
                             }
                         }
+                    }
+                    @Override
+                    public void onDrawerSlide(float openRatio,
+                            int offsetPixels) {
                     }
                 });
 
